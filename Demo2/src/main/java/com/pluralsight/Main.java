ebf6c0cece3ad7f34704add02a559c76c1f32f4b
mattchristenson <matt@rcc.team>
2025-06-10 07:50:13 -0500

    tues morning

--- Demo2/src/main/java/com/pluralsight/Main.java
+++ Demo2/src/main/java/com/pluralsight/Main.java
@@ -6,11 +6,27 @@ import javax.sql.DataSource;
 
 public class Main {
     public static void main(String[] args) throws SQLException, ClassNotFoundException {
+        displayCities(103);
+    }
+
+
+    public static void displayCities(int countryId) throws SQLException, ClassNotFoundException  {
+
+        if (args.length != 2) {
+            System.out.println(
+                    "Application needs two arguments to run: " +
+                            "java com.pluralsight.Main <username> <password>");
+            System.exit(1);
+        }
+
+        // get the user name and password from the command line args
+        String username = args[0];
+        String password = args[1];
+
         System.out.println("Hello world!");
 
         String connectionString = "jdbc:mysql://localhost:3306/sakila";
-        String username = "user1";
-        String password = "password1234";
+
 
         // load the MySQL Driver
         Class.forName("com.mysql.cj.jdbc.Driver");
@@ -19,15 +35,22 @@ public class Main {
         Connection connection;
         connection = DriverManager.getConnection(connectionString, username,password);
 
-        // create statement
-// the statement is tied to the open connection
-        Statement statement = connection.createStatement();
 
         // define your query
         String query = "SELECT city FROM city " +
-                "WHERE country_id = 103";
+                "WHERE country_id = ?";
+
+        // create statement
+// the statement is tied to the open connection
+
+
+        PreparedStatement ps = connection.prepareStatement(query);
+        ps.setInt(1, countryId);
+
+
+
 // 2. Execute your query
-        ResultSet results = statement.executeQuery(query);
+        ResultSet results = ps.executeQuery();
 
 // process the results
         while (results.next()) {
@@ -35,7 +58,14 @@ public class Main {
             System.out.println(city);
         }
 // 3. Close the connection
+        results.close();
+        ps.close();
         connection.close();
 
     }
+
+
 }
+
+
+
