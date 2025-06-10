9eac7939ca9ffa6e3f6709e47957c81390f1edc9
mattchristenson <matt@rcc.team>
2025-06-10 13:34:54 -0500

    example of home made basicdatasource

--- Demo2/src/main/java/com/pluralsight/BasicDataSource.java
+++ Demo2/src/main/java/com/pluralsight/BasicDataSource.java
@@ -1,12 +1,16 @@
 package com.pluralsight;
 
-public class sqlConnectionInfo {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class BasicDataSource {
 
     private String connectionString;
     private String username;
     private String password;
 
-    public sqlConnectionInfo(String connectionString, String username, String password) {
+    public BasicDataSource(String connectionString, String username, String password) {
         this.connectionString = connectionString;
         this.username = username;
         this.password = password;
@@ -34,5 +38,20 @@ public class sqlConnectionInfo {
 
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    public Connection getConnection() throws SQLException {
+        try{
+            // load the MySQL Driver
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            return  DriverManager.getConnection( this.getConnectionString(),
+                    this.getUsername(), this.getPassword());
+
+
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+
+
     }
 }
--- Demo2/src/main/java/com/pluralsight/Main.java
+++ Demo2/src/main/java/com/pluralsight/Main.java
@@ -1,12 +1,10 @@
 package com.pluralsight;
 
-import com.mysql.cj.jdbc.MysqlDataSource;
 import java.sql.*;
-import javax.sql.DataSource;
 
 public class Main {
 
-    private static sqlConnectionInfo sqlConnectionInfo;
+    private static BasicDataSource basicDataSource;
 
     public static void main(String[] args) {
 
@@ -17,7 +15,7 @@ public class Main {
             System.exit(1);
         }
 
-        sqlConnectionInfo = getSqlConnectionInfoFromArgs(args);
+        basicDataSource = getBasicDataSourceFromArgs(args);
 
         try{
             displayCities(103);
@@ -30,24 +28,20 @@ public class Main {
 
     }
 
-    public static sqlConnectionInfo getSqlConnectionInfoFromArgs(String[] args){
+    public static BasicDataSource getBasicDataSourceFromArgs(String[] args){
         // get the user name and password from the command line args
         String username = args[0];
         String password = args[1];
 
         String connectionString = args[2];
 
-        return new sqlConnectionInfo(connectionString, username, password);
+        return new BasicDataSource(connectionString, username, password);
     }
 
 
     public static void displayCities(int countryId) throws SQLException, ClassNotFoundException {
 
-        // load the MySQL Driver
-        Class.forName("com.mysql.cj.jdbc.Driver");
-
-
-        try (Connection connection = DriverManager.getConnection( sqlConnectionInfo.getConnectionString(), sqlConnectionInfo.getUsername(), sqlConnectionInfo.getPassword());
+        try (Connection connection = basicDataSource.getConnection();
              PreparedStatement ps = connection.prepareStatement("SELECT city FROM city WHERE country_id = ?");)
         {
             ps.setInt(1, countryId);
@@ -58,10 +52,17 @@ public class Main {
                     System.out.println(city);
                 }
             }
+            finally{
+                //close the results ResultSet
+            }
 
 
         } catch (Exception e) {
             e.printStackTrace();
+        }
+        finally {
+            //close the ps PreparedStatement
+            //close the connection Connection
         }
 
     }
@@ -72,7 +73,7 @@ public class Main {
         Class.forName("com.mysql.cj.jdbc.Driver");
 
 
-        try (Connection connection = DriverManager.getConnection( sqlConnectionInfo.getConnectionString(), sqlConnectionInfo.getUsername(), sqlConnectionInfo.getPassword());
+        try (Connection connection = DriverManager.getConnection( basicDataSource.getConnectionString(), basicDataSource.getUsername(), basicDataSource.getPassword());
              PreparedStatement ps = connection.prepareStatement("SELECT city FROM city where country_id");
              ResultSet results = ps.executeQuery();
              )
@@ -84,6 +85,11 @@ public class Main {
 
         } catch (Exception e) {
             e.printStackTrace();
+        }
+        finally {
+            //close the resultset
+            //close the prepared statement
+            //close the connection
         }
 
     }
